2882811a24e7a9d58ca2af04caca7fc4b252afc2
ponzu840w <ponzu840w@yahoo.co.jp>
2025-05-16 01:49:25 +0900

    ホットキーが発動するメソッドのプラットフォーム分離

--- Platforms/Common/HotKeyManager.cs
+++ Platforms/Common/HotKeyManager.cs
@@ -5,64 +5,43 @@ using Shapoco.Calctus;
 using Shapoco.Calctus.UI;
 
 namespace Shapoco.Platforms.Common {
-    internal partial class HotKeyManager {
-        readonly IHotKeyService _service;
-        readonly MainForm _mainForm;
-        readonly NotifyIcon _notifyIcon;
+  internal partial class HotKeyManager {
+    readonly IHotKeyService _service;    // ホットキーの登録・解除（マルチプラットフォーム）
+    readonly IWindowPopupToggle _winpop; // ウィンドウの出没（マルチプラットフォーム）
 
-        [DllImport("user32.dll")]
-        public static extern IntPtr GetForegroundWindow();
-
-        public HotKeyManager(MainForm mf, NotifyIcon notifyIcon = null) {
-            _mainForm = mf; _notifyIcon = notifyIcon;
-            if (Platform.IsWindows()) {
-                _service = new Windows.WindowsHotKeyService();
-            } else if (Platform.IsLinuxMono()) {
-                _service = new Linux.LinuxHotKeyService();
-            } else {
-                throw new PlatformNotSupportedException();
-            }
-            _service.HotKeyPressed += OnHotKey;
-        }
-
-        public void Enable() {
-            var s = Settings.Instance;
-            if (!s.Hotkey_Enabled) return;
-            if (!_service.Register(
-                  (ModifierKey)(
-                    (s.HotKey_Ctrl  ? ModifierKey.Ctrl  : 0) |
-                    (s.HotKey_Alt   ? ModifierKey.Alt   : 0) |
-                    (s.HotKey_Shift ? ModifierKey.Shift : 0) |
-                    (s.HotKey_Win   ? ModifierKey.Win   : 0)),
-                  s.HotKey_KeyCode))
-                MessageBox.Show("Hotkey registration failed.");
-        }
-
-        public void Disable() => _service.Unregister();
-
-        private void OnHotKey(object sender, EventArgs e)
-        {
-            if (GetForegroundWindow() == _mainForm.Handle)
-            {
-                if (_notifyIcon.Visible)
-                {
-                    _mainForm.Visible = false;
-                }
-                else
-                {
-                    _mainForm.WindowState = FormWindowState.Minimized;
-                }
-            }
-            else
-            {
-                _mainForm.ShowForeground();
-            }
-        }
+    // プラットフォーム依存部分はコンストラクタで実装を注入
+    public HotKeyManager(MainForm mf, NotifyIcon notifyIcon = null) {
+      if (Platform.IsWindows()) {
+        _service = new Windows.WindowsHotKeyService();
+        _winpop = new Windows.WindowsWindowPopupToggle(mf, notifyIcon);
+        _service.HotKeyPressed += _winpop.Toggle;
+      } else if (Platform.IsLinuxMono()) {
+        _service = new Linux.LinuxHotKeyService();
+      } else {
+        throw new PlatformNotSupportedException();
+      }
     }
 
-    [Flags]
-    public enum ModifierKey {
-        None = 0, Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+    // 設定を読んでホットキーを登録する
+    public void Enable() {
+      var s = Settings.Instance;
+      if (!s.Hotkey_Enabled) return;
+      if (!_service.Register(
+            (ModifierKey)(
+              (s.HotKey_Ctrl  ? ModifierKey.Ctrl  : 0) |
+              (s.HotKey_Alt   ? ModifierKey.Alt   : 0) |
+              (s.HotKey_Shift ? ModifierKey.Shift : 0) |
+              (s.HotKey_Win   ? ModifierKey.Win   : 0)),
+            s.HotKey_KeyCode))
+        MessageBox.Show("Hotkey registration failed.");
     }
 
+    // ホットキーを解除する
+    public void Disable() => _service.Unregister();
+  }
+
+  [Flags]
+  public enum ModifierKey {
+    None = 0, Alt = 1, Ctrl = 2, Shift = 4, Win = 8
+  }
 }
---
+++ Platforms/Common/IWindowPopupToggle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Shapoco.Calctus;
+using Shapoco.Calctus.UI;
+
+namespace Shapoco.Platforms.Common {
+  public interface IWindowPopupToggle {
+    /// <summary>ウィンドウの出没</summary>
+    void Toggle(object sender, EventArgs e);
+  }
+}
