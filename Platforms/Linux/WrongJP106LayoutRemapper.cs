bf615885979e9bf22b43e97e163cde61956368ca
ponzu840w <ponzu840w@yahoo.co.jp>
2025-05-31 23:36:29 +0900

    [add]jp106パッチのマッピングを拡充

--- Platforms/Linux/WrongJP106LayoutRemapper.cs
+++ Platforms/Linux/WrongJP106LayoutRemapper.cs
@@ -11,11 +11,31 @@ namespace Shapoco.Platforms.Linux {
   {
     private static readonly Dictionary<Keys, Keys> _mapping = new Dictionary<Keys, Keys>
     {
-      { Keys.Q, Keys.A },
-      { Keys.W, Keys.Z },
-
-      { Keys.A, Keys.Q },
-      { Keys.Z, Keys.W },
+      { Keys.Q, Keys.A }, { Keys.A, Keys.Q },
+      { Keys.W, Keys.Z }, { Keys.Z, Keys.W },
+      { Keys.Oem7,  Keys.D1 },
+      { Keys.D1,    Keys.D2 },
+      { Keys.D2,    Keys.D3 },
+      { Keys.D3,    Keys.D4 },
+      { Keys.D4,    Keys.D5 },
+      { Keys.D5,    Keys.D6 },
+      { Keys.D6,    Keys.D7 },
+      { Keys.D7,    Keys.D8 },
+      { Keys.D8,    Keys.D9 },
+      { Keys.D9,    Keys.D0 },
+      { Keys.D0,    Keys.OemMinus },
+      { Keys.Oem4,              Keys.Oem7 },
+      //{ Keys.Oem102,            Keys.Oem5 },  // 円記号はバックスラッシュと同一視される
+      { Keys.OemCloseBrackets,  Keys.Oemtilde },
+      { Keys.Oem1,              Keys.OemOpenBrackets },
+      { Keys.M,                 Keys.Oemplus },
+      { Keys.Oemtilde,          Keys.Oem1 },
+      { Keys.OemPipe,           Keys.OemCloseBrackets },
+      { Keys.Oemcomma,          Keys.M },
+      { Keys.OemPeriod,         Keys.Oemcomma },
+      { Keys.OemQuestion,       Keys.OemPeriod },
+      { Keys.Oem8,              Keys.OemQuestion },
+      { Keys.Oem102,            Keys.OemBackslash },
     };
 
     /// <summary>
